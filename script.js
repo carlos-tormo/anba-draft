b6f09b103d01d82db00224e2c84dd1421736f5ca
carlos-tormo <110094086+carlos-tormo@users.noreply.github.com>
2024-05-11 22:39:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     boxes.forEach((box, index) => {
         box.addEventListener('click', function() {
-            // Set background image from data attribute
-            const imageUrl = this.getAttribute('data-image-url');
-            this.style.backgroundImage = `url('${imageUrl}')`;
-            this.style.backgroundSize = 'cover';
-            this.children[0].style.display = 'none'; // Hide the number
+            // Check if the box already has a background image set
+            if (!this.style.backgroundImage) {
+                const imageUrl = this.getAttribute('data-image-url');
+                this.style.backgroundImage = `url('${imageUrl}')`;
+                this.style.backgroundSize = 'cover';
+                this.children[0].style.display = 'none'; // Hide the number
 
-            // Fade out the corresponding image in the row
-            if (rowImages[index]) { // Check if the image exists
-                rowImages[index].style.opacity = '0';
+                // Fade out the corresponding image in the row
+                if (rowImages[index]) { // Check if the corresponding image exists
+                    rowImages[index].style.opacity = '0';
+                }
             }
         });
     });
