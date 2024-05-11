e4852adee64b0faa2a936b449cde4db267bedf81
carlos-tormo <110094086+carlos-tormo@users.noreply.github.com>
2024-05-11 22:36:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const boxes = document.querySelectorAll('.box');
+    const rowImages = document.querySelectorAll('.image-row-item');
 
-    boxes.forEach(box => {
+    boxes.forEach((box, index) => {
         box.addEventListener('click', function() {
+            // Set background image from data attribute
             const imageUrl = this.getAttribute('data-image-url');
             this.style.backgroundImage = `url('${imageUrl}')`;
             this.style.backgroundSize = 'cover';
             this.children[0].style.display = 'none'; // Hide the number
+
+            // Fade out the corresponding image in the row
+            if (rowImages[index]) { // Check if the image exists
+                rowImages[index].style.opacity = '0';
+            }
         });
     });
 });
